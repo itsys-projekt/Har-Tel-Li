3387271b6cd78316f6d879093227bdd2fbe579e9
Hydoc <felixsehnert@yahoo.de>
2019-02-28 10:38:13 +0100

    ItSys-Proj: -> File Splitting

--- alternate_version _1/MenuSwipe/JavaScript.js
+++ alternate_version _1/MenuSwipe/JavaScript.js
@@ -1,9 +1,61 @@
-﻿//var five_star_ratig = innerHTML = "<fieldset class='rating'></fieldset>"
-function insertAfter(referenceNode, newNode) {
+﻿function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
+let _C = document.querySelector('.container');
+N = _C.children.length;
 
+let i = 0,
+    x0 = null,
+    locked = false;
+
+function unify(e) {
+    return e.changedTouches ? e.changedTouches[0] : e;
+}
+
+function lock(e) {
+    x0 = unify(e).clientX;
+
+    _C.classList.toggle('smooth', !(locked = true));
+}
+
+function drag(e) {
+    e.preventDefault();
+
+    if (locked) {
+        _C.style.setProperty('--tx', ''.concat(Math.round(unify(e).clientX - x0), 'px'));
+    }
+}
+
+function move(e) {
+    if (locked) {
+        let dx = unify(e).clientX - x0,
+            s = Math.sign(dx);
+        if ((i > 0 || s < 0) && (i < N - 1 || s > 0)) _C.style.setProperty('--i', i -= s);
+
+        _C.style.setProperty('--tx', '0px');
+
+        _C.classList.toggle('smooth', !(locked = false));
+
+        x0 = null;
+    }
+}
+
+_C.style.setProperty('--n', N);
+
+_C.addEventListener('mousedown', lock, false);
+
+_C.addEventListener('touchstart', lock, false);
+
+_C.addEventListener('mousemove', drag, false);
+
+_C.addEventListener('touchmove', drag, false);
+
+_C.addEventListener('mouseup', move, false);
+
+_C.addEventListener('touchend', move, false);
+
+//var five_star_rating = innerHTML = "<fieldset class='rating'></fieldset>"
 //function addStarRatings() {
 
 //    var fieldset = document.createElement("fieldset");
